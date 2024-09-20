03a80af3a03e1fa4f203a4d9fe8acbffec218d18
Volha <kresiko@gmail.com>
2024-09-20 21:51:11 +0300

    init: first version of audio-player

---
+++ audio-player/index.js
@@ -0,0 +1,107 @@
+const player = document.querySelector('.player');
+const playBtn = document.querySelector('.play');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const audio = document.querySelector('audio');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress__container');
+const cover = document.querySelector('.cover');
+const title = document.querySelector('.title');
+const imgSrc = document.querySelector('.img__src');
+const wrapper = document.querySelector('.wrapper');
+
+//song names
+songs = ["Song1", "Song2"];
+
+//default song
+songIndex = 0;
+
+function loadSong(song) {
+    title.innerHTML = song;
+    let curInd = songIndex + 1;
+    audio.src = 'audio/song' + curInd + '.mp3';
+    cover.src = "images/image" + curInd + ".png";
+    let bkgrndurl = `images/image${curInd}.png`;
+    // player.style.backgroundImage = "url(`img/image${curInd}.png`)";
+    console.log(`url(${bkgrndurl})`);
+    player.style.backgroundImage = `url(${bkgrndurl})`;
+    wrapper.style.backgroundImage = `url(${bkgrndurl})`;
+}
+
+loadSong(songs[songIndex]);
+
+//play
+function playSong() {
+    player.classList.add('play');
+    imgSrc.src = "icons/pause.png";
+    audio.play();
+}
+
+function pauseSong() {
+    player.classList.remove('play');
+    imgSrc.src = "icons/play.png";
+    audio.pause();
+}
+
+playBtn.addEventListener('click', () => {
+    const isPlaying = player.classList.contains('play');
+    console.log(player.classList);
+
+    if (isPlaying) {
+        pauseSong();
+    }
+    else {
+        playSong();
+    }
+});
+
+
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+nextBtn.addEventListener('click', nextSong);
+// playSong()
+
+function prevSong() {
+    songIndex--;
+    console.log("SongsIndex " + songIndex);
+    if (songIndex < 0) {
+        console.log("song index <0> ");
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+prevBtn.addEventListener('click', prevSong);
+
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+
+    progress.style.width = `${progressPercent}% `;;
+    // `${ progressPercent }% `;
+
+}
+audio.addEventListener('timeupdate', updateProgress);
+
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width) * duration;
+}
+
+progressContainer.addEventListener('click', setProgress);
+
+//autoplay
+
+audio.addEventListener('ended', nextSong);
